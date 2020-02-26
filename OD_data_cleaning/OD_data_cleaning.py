42ed99ddebeef7b43dedf184ae6e4c542f493864
Felix Pacheco Pastor <felixpacheco96@gmail.com>
2020-02-26 15:58:31 +0100

    Implementation of the volume loss estimation
    -- There is still corrections to do (the layout of the plate is not clear, so the linear model looks bad for now)

--- OD_data_cleaning/OD_data_cleaning.py
+++ OD_data_cleaning/OD_data_cleaning.py
@@ -1,9 +1,20 @@
 ### Libraries ###
 import pandas as pd
 from datetime import datetime
+import croissance
 from croissance import process_curve
 from croissance.estimation.outliers import remove_outliers
 import re
+import os
+import matplotlib.pyplot as plt 
+import matplotlib
+from scipy import interpolate
+import numpy as np
+from scipy.optimize import curve_fit
+from croissance.estimation.util import with_overhangs
+from croissance.estimation import regression
+
+
 
 # ------ DATA LOADING AND VARIABLE SELECTION ------
 
@@ -35,9 +46,12 @@ def sample_outcome(sample_file, df_raw) :
 
 	#Separate initial dataframe in 2
 	df_gr = df_raw[df_raw.Sample_ID.isin(gr_samples)]
+	df_gr = df_gr.loc[df_gr["Measurement_type"] == "OD600"]
 	df_vl = df_raw[df_raw.Sample_ID.isin(vol_loss_samples)]
 	
 	return df_gr, df_vl
+
+
 
 
 # ------- GROWTH RATE FORMATTING FOR SUITABLE CROISSANCE ANALYSIS INPUT ------
@@ -46,7 +60,6 @@ def gr_time_format(df_gr) :
 	''' discards non relevant OD measurements, turns date and time variables into differential time in hours, returns modified dataframe'''
 
 	#Select rows with only a OD600 measurement 
-	df_gr = df_gr.loc[df_gr["Measurement_type"] == "OD600"]
 	#Measurement type variable removal
 	df_gr = df_gr.drop(columns=["Measurement_type"])
 
@@ -62,6 +75,52 @@ def gr_time_format(df_gr) :
 	df_gr = df_gr.drop(columns=["date_time"])
 	
 	return df_gr
+
+# ------ VOLUME LOSS COMPENSATION FOR EVERY SAMPLE ------
+
+def vol_correlation(df_gr, df_vl):
+	''' Assess the volume loss with OD450 measurements and compesate the OD600 growth rate readings for every different bioshaker'''
+	#Subset initial df_raw according to OD measurement and get unique IDs
+	df_vl = df_vl.loc[df_vl["Measurement_type"] == "OD450"]
+	unique_IDs_vl = df_vl["Sample_ID"].unique()
+	df_vl["bioshaker"] = df_vl["Sample_ID"].str[0:3]
+	unique_bioshaker = df_vl["bioshaker"].unique()
+	cor_df = pd.DataFrame()
+
+
+	for pos in range(len(unique_IDs_vl)) :
+		df_vl_ID = df_vl.loc[df_vl["Sample_ID"] == unique_IDs_vl[pos]]
+		init_val = float((df_vl_ID["Measurement"].tolist()[0]))
+		df_vl_ID["Correlation"] = df_vl_ID["Measurement"]/init_val
+		''' # This is done because I spotted something in the data and wanted to get rid of some noisy obs
+		if df_vl_ID["Correlation"].max() < 5 :
+			df_vl_ID = pd.DataFrame()
+		
+		else : 
+			cor_df = cor_df.append(df_vl_ID)
+			df_vl_ID = pd.DataFrame()
+		'''
+		cor_df = cor_df.append(df_vl_ID)
+		df_vl_ID = pd.DataFrame()
+	cor_df = gr_time_format(cor_df)
+	#plt.scatter(cor_df["time_hours"],cor_df["Correlation"])
+	return cor_df
+
+def compensation_lm(cor_df) :
+	unique_bioshaker = cor_df["bioshaker"].unique()
+	linear = lambda x, a, b: a * x + b
+	fig, axes = plt.subplots(nrows=2, ncols=2)
+	fig.subplots_adjust(hspace=0.4, wspace=0.4)
+	fig.suptitle('Linear models of volume loss correlation to time for different plates')
+	for shaker in range(len(unique_bioshaker)) :
+		sub_cor_df = cor_df[cor_df["bioshaker"]== unique_bioshaker[shaker]]
+		popt, pcov = curve_fit(linear, sub_cor_df ["time_hours"], sub_cor_df["Correlation"], p0=[1, 1])
+		ax = fig.add_subplot(2, 2, shaker+1)
+		ax.plot(sub_cor_df["time_hours"], sub_cor_df["Correlation"], "rx")
+		ax.plot(sub_cor_df["time_hours"], linear(sub_cor_df ["time_hours"], *popt), "b-")
+	
+	return plt.show()
+
 
 
 # ------ RESHAPE GROWTH RATE DATAFRAME FOR CROISSANCE INPUT ------
@@ -88,57 +147,34 @@ def reshape_gr(df_gr) :
 
 # --- AUTOMATED PLOT GENERATION FOR EVERY SAMPLE
 
+#Attempt to do a linear model
+def func(x, a, b, c):
+	return a * np.exp(b * x) + c
+
 def gr_plots(df_gr_final) :
-	''' removes outliers for every sample and outputs a growth curve plot for every sample ID'''
-	for 
-		my_series = pd.Series(data = (df_gr_final["BS1_D1"]).tolist(), index=(df_gr_final["timeBS1_D1"]).tolist())
-		outliers = remove_outliers(my_series, window=30, std=2)
+	''' removes outliers for every sample and outputs a growth curve plot for a given sample ID'''
+	
+	#OPTION 1 : USE CROISSANCE
+	#croissance series input format 
+	#The idea is to produce the gr plots in a for loop interating through the datafrane every 2 cols
+	#To start with we try it with one column
+	my_series = pd.Series(data = (df_gr_final["BS1_A1"]).tolist(), index=(df_gr_final["timeBS1_A1"]).tolist())
+	#gr_estimation = process_curve(my_series)  #This gives an error 
+	#OPTION 2 :  USE CROISSANCE TO REMOVE OUTLIERS AND THEN PLOT THE SMOOTH CURVE WITH INTERPOLATION 
+	'''
+	clean_series = remove_outliers(my_series)[0]	#Extract series withoyt outliers
+	df = pd.DataFrame({"time":clean_series.index, "BS1_A1":clean_series.values})
+	x_new = np.linspace(df["time"].min(),df["time"].max(),500)
+	a_BSpline = interpolate.make_interp_spline(df["time"], df["BS1_A1"])
+	y_new = a_BSpline(x_new)
+	sd_pos = y_new+np.std(df["BS1_A1"])
+	sd_neg = y_new-np.std(df["BS1_A1"])
+	plt.plot(x_new,y_new)
+	fig = plt.scatter(df["time"],df["BS1_A1"],5, facecolor=(.18, .31, .31))
+	plt.fill_between(x_new, sd_pos, sd_neg, facecolor="red", color="dodgerblue", alpha=0.3)
+	plt.ylabel('Absorbance (OD)', fontname="Arial", fontsize=12)
+	plt.xlabel('Time (h)', fontname="Arial", fontsize=12)
+	plt.title("Growth rate curve for sample BS1_A1", fontname="Arial", fontsize=12)
+	'''
+	return my_series
 
-
-
-
-
-
-
-
-
-
-
-'''
-def gr_plots(df_gr_final) :
-
-	#Turn dataframe columns into series
-
-#my_series = pd.Series(data = (df_gr_final["BS1_D1"]).tolist(), index=(df_gr_final["timeBS1_D1"]).tolist())
-#result = process_curve(my_series)
-
-#This is a piece of code that generates a series that works, I do not see the difference between this data and my_series
-#This was taken from the test.py of the croissance repository ("https://github.com/meono/croissance/blob/master/test.py")
-mu = .20
-pph = 4.
-my_series = pd.Series(
-                    data=([i / 10. for i in range(10)] +
-                          [np.exp(mu * i / pph) for i in range(25)] +
-                          [np.exp(mu * 24 / pph)] * 15),
-                    index=([i / pph for i in range(50)]))
-
-#This gives an error on my_series but not on data1
-
-
-result = process_curve(my_series)
-print(result)
-
-
-
-#result = AnnotatedGrowthCurve(my_series, outliers = remove_outliers(my_series, window=30, std=2),  growth_phases = )
-#print(result.growth_phases)
-
-
-
-# ------ VOLUME LOSS COMPENSATION ------
-
-#Select rows with only a OD450 measurement 
-df_vl = df_vl.loc[df_vl["Measurement_type"] == "OD450"]
-df_vl 
-#print(df_vl)
-'''
--- OD_data_cleaning/__main__.py
+++ OD_data_cleaning/__main__.py
@@ -1,28 +1,66 @@
 import sys
 import pandas as pd 
-from OD_data_cleaning import read_xlsx, sample_outcome, gr_time_format, reshape_gr
+from OD_data_cleaning import read_xlsx, sample_outcome, gr_time_format, reshape_gr,gr_plots, vol_correlation, compensation_lm
 from croissance.estimation.outliers import remove_outliers
+import matplotlib.pyplot as plt
+import croissance
+from croissance import process_curve
+from croissance.estimation import AnnotatedGrowthCurve
+from croissance.figures import PDFWriter
+import numpy as np
+
+
+
+
+
+#OD_DATA_CEALING
+import pandas as pd
+from datetime import datetime
+import croissance
+from croissance import process_curve
+from croissance.estimation.outliers import remove_outliers
+import re
+import os
+import matplotlib.pyplot as plt 
+import matplotlib
+from scipy import interpolate
+import numpy as np
+from scipy.optimize import curve_fit
+from croissance.estimation.util import with_overhangs
+from croissance.estimation import regression
+
+
+
 
 try :
 	if sys.argv[1] == "-h" or sys.argv[1] == "--help" :
-		print('''		usage: OD_data_clean <ODS_file.xlxs>  <sample_purpose.tsv>\n		help: OD_data_clean [-h]  [--help]''')
+		print('''		usage: OD_data_clean <autoflow_parser_output_file.xlxs>  <sample_purpose.tsv>\n		help: OD_data_clean [-h]  [--help]''')
 		sys.exit(1)
 	elif len(sys.argv) == 3 :
 		if sys.argv[1].endswith(".xlsx") and sys.argv[2].endswith(".tsv") :
 			ODS_file = sys.argv[1]
 			sample_purpose = sys.argv[2]
-			print("a")
 
 	else :
-		sys.exit("usage: OD_data_clean <file.xlxs>  <sample_purpose.tsv>")
+		sys.exit("usage: OD_data_clean <autoflow_parser_output_file.xlxs>  <sample_purpose.tsv>")
 except IndexError :
-	sys.exit("IndexError!\nusage: OD_data_clean <file.xlxs>  <sample_purpose.tsv>")
+	sys.exit("IndexError!\nusage: OD_data_clean <autoflow_parser_output_file.xlxs>  <sample_purpose.tsv>")
 
 #Read data file
 df_raw = read_xlsx(ODS_file)
-print(df_raw)
+
 #Separate data depending on sample purpose (growth rate or volume loss)
 df_gr, df_vl = sample_outcome(sample_purpose, df_raw)
+'''
+print(len(df_gr["Sample_ID"].unique()))
+print("----")
+print(len(df_vl["Sample_ID"].unique()))
+'''
+cor_df = vol_correlation(df_gr, df_vl)
+
+compensation_lm(cor_df)
+
+
 
 #Change time format to hours
 df_gr = gr_time_format(df_gr)
@@ -30,6 +68,27 @@ df_gr = gr_time_format(df_gr)
 #Reshape data for croissance input
 df_gr_final = reshape_gr(df_gr)
 
-my_series = pd.Series(data = (df_gr_final["BS1_D1"]).tolist(), index=(df_gr_final["timeBS1_D1"]).tolist())
-outliers = remove_outliers(my_series, window=30, std=2)
-print(outliers)
+#print(gr_plots(df_gr_final))
+
+
+#This is a piece of code that generates a series that works, I do not see the difference between this data and my_series
+#This was taken from the test.py of the croissance repository ("https://github.com/meono/croissance/blob/master/test.py")
+mu = .20
+pph = 4.
+data1 = pd.Series(
+                    data=([i / 10. for i in range(10)] +
+                          [np.exp(mu * i / pph) for i in range(25)] +
+                          [np.exp(mu * 24 / pph)] * 15),
+                    index=([i / pph for i in range(50)]))
+
+
+
+
+
+
+
+
+
+
+
+

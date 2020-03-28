2cebfe7e974158ee4c0e5640982c505c05bc7c47
Felix Pacheco Pastor <felixpacheco96@gmail.com>
2020-03-28 09:02:23 +0100

    Add files via upload

--- OD_handler/OD_handler.py
+++ OD_handler/OD_handler.py
@@ -19,46 +19,124 @@ from pandas import Series
 import subprocess
 import sys
 from scipy import interpolate
+import math
+import argparse
 
 __version__ = "0.0.1"
 
 
+
+# ------ MAIN RUNNER ------
+
+
+def argument_parser(argv_list=None):
+	'''Asesses the input arguments and outputs flags to run the different functions according to the user needs.
+	Keyword arguments:
+    argv_list -- List of arguments provided by the user when running the program.
+    Returned variables:
+    flags
+	'''
+
+	#Initialize the argument parser
+	parser = argparse.ArgumentParser()
+
+	#Adding
+	parser.add_argument("-e", "--estimations", help="Get only the estimations for every sample", action = "store_true")
+	parser.add_argument("-f", "--figures", help="Get only the growth curve figures", action = "store_true")
+	parser.add_argument("-s", "--summary", help="Get only the summary of growth rate estimations",action = "store_true")
+	parser.add_argument("-b", "--bioshaker", help="Get one growth rates figures for every individual bioshaker", action = "store_true")
+	parser.add_argument("-i", "--individual", help="Get one growth rates figures for every individual sample", action = "store_true")
+	parser.parse_args()
+	args = parser.parse_args(argv_list[1:])
+
+	#Create flags 
+
+	if args.estimations == False and args.figures == False and args.individual == False and  args.summary == False :
+		
+		flag_all = True
+		flag_est = False
+		flag_sum = False
+		flag_fig = False
+		flag_ind = False
+		flag_bioshaker = False
+
+	elif args.estimations == False and args.figures == False and args.individual == True and  args.summary == False :
+
+		flag_all = True
+		flag_est = False
+		flag_sum = False
+		flag_fig = False
+		flag_ind = True
+		flag_bioshaker = False
+
+	else :
+
+		flag_all = False
+		flag_est = args.estimations
+		flag_sum = args.summary
+		flag_fig = args.figures
+		flag_ind = args.individual
+		flag_bioshaker = args.bioshaker
+
+	return flag_all, flag_est, flag_sum, flag_fig, flag_ind, flag_bioshaker
+
+
+
 # ------ PARSE THE DATA USING THE autoflow_parser LIBRARY ------
+
+
 def parse_data() :
+	'''Calls the autoflow_parser and returns a merged xlsx document with all the OD readings combined'''
 	try :
 		call = subprocess.call("autoflow_parser",shell = True)
 	except :
 		return sys.exit("The data could not be parsed due to some error, check the input documentation")
 	return call
 
+
+
 # ------ DATA LOADING AND VARIABLE SELECTION ------
 
-def read_xlsx() :
-	'''Reads .xlsx file, returns a dataframe with relevant variables'''
+
+def read_xlsx(filename = "results.xlsx") : #done
+	'''Reads .xlsx file, returns a dataframe with relevant variables. The output of the parser is set to be "results.xlsx", the default reads the mentioned file without any additional argument'''
 	try :
 		#Open data file
-		df_raw = pd.read_excel("results.xlsx")
+		df = pd.read_excel(filename)
 	except FileNotFoundError :
 		return sys.exit("Could not find the parsed data file (XLSX extension)")
 	#Select valuable columns
 	cols = ["Sample ID", "Measurement", "Measurement type", "Sampling time","Sampling date"]	#relevant variables
-	df_raw = df_raw[cols]
-	df_raw.columns = df_raw.columns.str.replace(' ', '_')	#header spaces replaced with underscore
+	df = df[cols]
+	df.columns = df.columns.str.replace(' ', '_')	#header spaces replaced with underscore
 	
-	return df_raw
+	return df
+
 
 
 # ------ SEPARATION OF GROWTH RATE SAMPLE AND VOLUME LOSS SAMPLES ------
 
-def sample_outcome(sample_file, df_raw) :
-	''' Uses an external file containing individual sample purposes, returns two classifed dataframes based on sample purposes '''
-	#Open the file containing sample purposes
-	df_calc = pd.read_csv(sample_file, sep="\t")		#Info about the purpose of the sample (growth rate or volume loss compensation)
+
+def sample_outcome(sample_file, df) : #done
+	'''Uses an external file containing individual sample purposes, returns two classifed dataframes based on sample purposes and labelled by bioshaker.
+	Keyword arguments:
+    sample_file -- variable or string containing the name of the file and its extension.
+    df 			-- dataframe obtained by using the read_xlsx method on the merged xlsx file.
 	
-	#add species to every observation
+	Returned variables:
+	df_gr -- dataframe containing observations related to the microbial growth rate, labelled by bioshaker.
+	df_vl -- dataframe containing observations related to the volume loss estimation, labelled by bioshaker.
+    '''
+
+	#Open the file containing sample purposes
+	df_calc = pd.read_csv(sample_file, sep="\t")  #Info about the purpose of the sample (growth rate, volume loss compensation, species and drop-out samples)
+	df_calc = df_calc[df_calc.Drop_out == False]
+	
+	#Add species and bioshaker labels to every observation
 	cols=["Sample_ID", "Species"]
 	temp_df = df_calc[cols]
-	df_raw = pd.merge(df_raw, temp_df, how="left", on="Sample_ID")
+	df = pd.merge(df, temp_df, how="left", on="Sample_ID")
+	df["bioshaker"] = df["Sample_ID"].str[0:3]
 
 	#Separate samples for growth rate or volume loss according to calc.tsv
 	gr_samples = df_calc.loc[df_calc["calc_gr"] == True]
@@ -67,68 +145,107 @@ def sample_outcome(sample_file, df_raw) :
 	vol_loss_samples = vol_loss_samples["Sample_ID"].tolist()
 
 	#Separate initial dataframe in 2
-	df_gr = df_raw[df_raw.Sample_ID.isin(gr_samples)]
+	df_gr = df[df.Sample_ID.isin(gr_samples)]
 	df_gr = df_gr.loc[df_gr["Measurement_type"] == "OD600"]
-	df_vl = df_raw[df_raw.Sample_ID.isin(vol_loss_samples)]
+	df_vl = df[df.Sample_ID.isin(vol_loss_samples)]
 	df_vl = df_vl.loc[df_vl["Measurement_type"] == "OD450"]
+	
 	return df_gr, df_vl
 
 
 
 # ------- GROWTH RATE FORMATTING FOR SUITABLE CROISSANCE ANALYSIS INPUT ------
 
-def gr_time_format(df_gr) :
-	''' discards non relevant OD measurements to growth rate estimation, turns date and time variables into differential time in hours, returns modified dataframe'''
 
-	#Select rows with only a OD600 measurement 
-	#Measurement type variable removal
-	df_gr = df_gr.drop(columns=["Measurement_type"])
-
-	#Merge date and time variable to datetime format
-	df_gr["date_time"] = df_gr["Sampling_time"]+" "+df_gr["Sampling_date"]
-	df_gr = df_gr.drop(columns=["Sampling_date", "Sampling_time"])
-	df_gr["date_time"] = pd.to_datetime(df_gr["date_time"])
-
-	#Substracting the time of the first obs to all obs
-	df_gr['time_hours'] = df_gr["date_time"] - df_gr.loc[df_gr.index[0], 'date_time']
-	df_gr["time_hours"] = df_gr["time_hours"].dt.total_seconds()/3600
-	#Removal of date_time temporary variable
-	df_gr = df_gr.drop(columns=["date_time"])
+def time_formater(df) :
+	'''Takes a dataframe and turns date and time variables into differential time in hours for every bioshaker, returns modified dataframe.
+	Keyword arguments:
+    df 		-- dataframe with containing date and time measurements.
 	
-	return df_gr
+	Returned variables:
+	df_out 	-- dataframe with differential time measurements in hours.
+	'''
+	#Get list of bioshakers
+	unique_bioshakers = df["bioshaker"].unique()
+	
+	#Measurement type variable removal
+	df = df.drop(columns=["Measurement_type"])
+	
+	#Initialize empty output dataframe
+	df_out = pd.DataFrame()
+
+	for bioshaker in unique_bioshakers :
+
+		#Subset initial dataframe by bioshaker
+		df_temp = df.loc[df["bioshaker"] == bioshaker]
+
+		#Merge date and time variable to datetime format
+		df_temp["date_time"] = df_temp["Sampling_time"]+" "+df_temp["Sampling_date"]
+		df_temp = df_temp.drop(columns=["Sampling_date", "Sampling_time"])
+		df_temp["date_time"] = pd.to_datetime(df_temp["date_time"])
+
+		#Substracting the time of the first obs to all obs
+		df_temp['time_hours'] = df_temp["date_time"] - df_temp.loc[df_temp.index[0], 'date_time']
+		df_temp["time_hours"] = df_temp["time_hours"].dt.total_seconds()/3600
+
+		#Append dataframes together
+		df_out = df_out.append(df_temp)
+
+	#Removal of date_time temporary variable
+	df_out = df_out.drop(columns=["date_time"])
+	
+	return df_out
 
 
 
-# ------ VOLUME LOSS COMPENSATION FOR EVERY SAMPLE ------
+# ------ VOLUME LOSS CORRELATION FOR EVERY SAMPLE ------
 
-def vol_correlation(df_gr, df_vl):
-	''' Assess the volume loss with OD450 measurements and compesate the OD600 growth rate readings for every different bioshaker, returns a correlation dataframe and a modified growth rate dataframe containing the bioshaker category'''
+
+def vol_correlation(df_gr, df_vl): #done
+	''' Assess the volume loss with OD450 measurements and correlates the OD450 readings to time for every different bioshaker, returns a correlation dataframe.
+	Keyword arguments:
+    df_gr	-- dataframe containing only growth rate measurements and differential time in hours.
+	df_vl	-- dataframe containing only volume loss measurements.
+	
+	Returned variables:
+	cor_df 	-- dataframe containing correlation values of the volume loss according to time measurements.
+	df_gr 	-- dataframe with bioshaker column added.
+	'''
 	#Subset initial df_raw according to OD measurement and get unique IDs
-	df_vl = df_vl.loc[df_vl["Measurement_type"] == "OD450"]
 	unique_IDs_vl = df_vl["Sample_ID"].unique()
-	df_vl["bioshaker"] = df_vl["Sample_ID"].str[0:3]
-	df_gr["bioshaker"] = df_gr["Sample_ID"].str[0:3]
 	unique_bioshaker = df_vl["bioshaker"].unique()
 	cor_df = pd.DataFrame()
 
-
+	#Compute correlation for every sample
 	for pos in range(len(unique_IDs_vl)) :
 		df_vl_ID = df_vl.loc[df_vl["Sample_ID"] == unique_IDs_vl[pos]]
 		init_val = float((df_vl_ID["Measurement"].tolist()[0]))
 		df_vl_ID["Correlation"] = df_vl_ID["Measurement"]/init_val
 		cor_df = cor_df.append(df_vl_ID)
 		df_vl_ID = pd.DataFrame()
-		cor_df = gr_time_format(cor_df)
-	return cor_df, df_gr
+	
+	return cor_df
 
 
 
-def compensation_lm(cor_df, df_gr) :
-	''' Given the correlation between volume and time, a linear model is built and plotted, the correction is applied to the growth measurements using the linear model, returns a figure with the LM and a dataframe with the corrected growth rate measurements'''
+# ------ VOLUME LOSS COMPENSATION FOR EVERY SAMPLE ------
+
+
+def compensation_lm(cor_df, df_gr) : #done
+	''' Given the correlation between volume and time, a linear model is built and plotted, the correction is applied to the growth measurements using the linear model, returns a figure with the LM and a dataframe with the corrected growth rate measurements.
+	Keyword arguments:
+    df_gr	-- dataframe containing only growth rate measurements and differential time in hours.
+	cor_df	-- dataframe containing correlation measures between volume loss and time for different bioshakers.
+	
+	Returned variables:
+	fig 			-- figure representing the linear model between the correlation and the time for every bioshaker.
+	df_gr_comp_out 	-- dataframe containing corrected growth rate measurements and differential time in hours.'''
+	
+
 	#For every bioshaker a linear model is created
 	unique_bioshaker = cor_df["bioshaker"].unique()
 	linear = lambda x, a, b: a * x + b
-	fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(10,10))
+	fig, axes = plt.subplots(nrows=math.ceil(len(unique_bioshaker)), ncols=math.ceil(len(unique_bioshaker)/2))
 	fig.subplots_adjust(hspace=0.4, wspace=0.4)
 	fig.suptitle('Linear models of volume loss correlation to time for different plates')
 	lm_eq = []
@@ -138,7 +255,7 @@ def compensation_lm(cor_df, df_gr) :
 		sub_cor_df = cor_df[cor_df["bioshaker"]== unique_bioshaker[shaker]]
 		popt, pcov = curve_fit(linear, sub_cor_df ["time_hours"], sub_cor_df["Correlation"], p0=[1, 1])
 		lm_eq.append(popt)
-		ax = fig.add_subplot(2, 2, shaker+1)
+		ax = fig.add_subplot(math.ceil(len(unique_bioshaker)), math.ceil(len(unique_bioshaker)/2), shaker+1)
 		ax.plot(sub_cor_df["time_hours"], sub_cor_df["Correlation"], "o")
 		ax.get_xaxis().set_visible(False)
 		ax.get_yaxis().set_visible(False)
@@ -160,8 +277,20 @@ def compensation_lm(cor_df, df_gr) :
 
 # ------ RESHAPE GROWTH RATE DATAFRAME FOR CROISSANCE INPUT ------
 
-def reshape_gr(df_gr, flag = False) :
-	''' Collects the times belonging to every sample and creates a time column relative to a specific sample, returns the modified dataframe '''
+
+def reshape_dataframe(df_gr, flag = False) :
+	''' Collects the times belonging to every sample and creates a time column relative to a specific sample, returns the modified dataframe.
+	Keyword arguments:
+    df_gr 	-- dataframe containing growth rate measurements and differential time in hours.
+	flag  	-- flag that corresponds to the presence of more than one species (True), False if only one species
+	Returned variables:
+	if flag is False 
+	df_gr -- dataframe with differential time measurements in hours displayed horizontally (one column containing the time measurements and one column contaning the OD measurements PER SAMPLE).
+
+	if flag is True :
+	df_gr_final 	 --  dataframe with differential time measurements in hours displayed horizontally (one column containing the time measurements and one column contaning the OD measurements PER SAMPLE).
+	df_gr_final_list -- list of dataframes originated from df_gr_final and split by common sample species
+	'''
 	df_gr_temp = df_gr
 	cols = ["Sample_ID", "Measurement", "Corrected_Measurement", "time_hours"]	#relevant variables
 	df_gr = df_gr[cols]
@@ -233,46 +362,102 @@ def reshape_gr(df_gr, flag = False) :
 		return df_gr_final, df_gr_final_list
 
 
-# --- AUTOMATED GROWTH ESTIMATION FOR EVERY SAMPLE
+
+
+# ----- AUTOMATED GROWTH ESTIMATION FOR EVERY SAMPLE  -----
+
 
 def gr_estimation(df_gr_final) :
-	''' removes outliers for every sample and outputs growth rate estimates for every given sample ID as a text file'''
+	''' removes outliers for every sample and outputs growth rate estimates for every given sample ID as a text file
+	Keyword arguments:
+    df_gr_final 	-- dataframe containing growth rate measurements and differential time in hours.
 	
-	#OPTION 1 : USE CROISSANCE
+	Returned variables:
+	estimations -- List of growth rate estimations for every sample in df_gr_final
+	errors 		-- due to croissance noise handling some samples can not be estimated and list of series is returned for every non-estimated sample
+	'''
+	
 	#croissance series input format 
-	#To start with we try it with one column
 	df_gr_est = df_gr_final.loc[:,~df_gr_final.columns.str.startswith('Raw')]
 	df_gr_est = df_gr_est.loc[:,~df_gr_est.columns.str.startswith('time')]
 	colnames = []
 	colnames = (df_gr_est.columns.values)
 	estimations = []
 	errors = []
+	est_IDs = []
+	err_IDs = []
+	
 	for col in range(len(colnames)):
 		my_series = pd.Series(data = (df_gr_final[colnames[col]]).tolist(), index= df_gr_final[colnames[col].replace("Corrected","time")].tolist())
+		
 		# Estimation for every sample
 		try :
 			gr_estimation = process_curve(my_series)
 			estimations.append(colnames[col])
 			estimations.append(gr_estimation)
+			IDs.append(colnames[col])
+		
 		except :
 			errors.append(colnames[col])
 			errors.append(remove_outliers(my_series)[0])
-
-	return estimations , errors
-
-def gr_plots(df, sample) :
-	''' generates a growth curve plot for a given series for common species, returns the plot'''
-	x_new = np.linspace(df["time"].min(),df["time"].max(),500)
-	a_BSpline = interpolate.make_interp_spline(df["time"], df[sample])
-	y_new = a_BSpline(x_new)
-	plt.figure()
-	plt.plot(x_new,y_new)
-	fig = plt.scatter(df["time"],df[sample],5, facecolor=(.18, .31, .31))
-	plt.ylabel('Absorbance (OD)', fontname="Arial", fontsize=12)
-	plt.xlabel('Time (h)', fontname="Arial", fontsize=12)
-	plt.title("Growth rate curve of "+str(sample), fontname="Arial", fontsize=12)
-	return fig, plt.savefig(sample+"_GR_curve.png")
+			err_IDs.append(colnames[col])
 	
+	return estimations , errors, est_IDs, err_IDs
+
+
+
+# ----- ESTIMATION WRITTER TO XLSX FILE -----
+#The writter function goes here
+
+def estimation_writer(estimation_list, error_list) :
+	'''Writes a xlsx file with the estimations for every sample and outputs the errors on a log file.
+	Keyword arguments:
+    estimation_list		-- list containing the estimations (output from gr_estimation).
+    sample 				-- list containing the non-estimated samples.	
+	Returned variables:
+	whatsoever
+	'''
+	pass
+	return None
+
+# ----- PLOTTING GROWTH RATE CURVE -----
+
+
+def gr_plots(df, sample, color_ = None, ind = False) :
+	'''Generates a growth curve plot for a given series for common species, returns the plot.
+	Keyword arguments:
+    df		-- dataframe containing differential times and OD measurements
+    sample 	-- 
+    ind 	-- flag that indicates to output individual plots if True or merged plots by sample species if False
+	
+	Returned variables:
+	fig 		-- object containing the figure
+	plt.savefig -- saving the figure as a png file
+	'''
+	
+	if ind == True :
+		x_new = np.linspace(df["time"].min(),df["time"].max(),500)
+		a_BSpline = interpolate.make_interp_spline(df["time"], df[sample])
+		y_new = a_BSpline(x_new)
+		plt.figure()
+		plt.plot(x_new,y_new)
+		fig = plt.scatter(df["time"],df[sample],5, facecolor=(.18, .31, .31))
+		plt.ylabel('Absorbance (OD)', fontname="Arial", fontsize=12)
+		plt.xlabel('Time (h)', fontname="Arial", fontsize=12)
+		plt.title("Growth rate curve of "+str(sample), fontname="Arial", fontsize=12)
+		return fig, plt.savefig(sample+"_GR_curve.png")
+
+	elif ind == False :
+
+		x_new = np.linspace(df["time"].min(),df["time"].max(),500)
+		a_BSpline = interpolate.make_interp_spline(df["time"], df[sample])
+		y_new = a_BSpline(x_new)
+		plt.plot(x_new,y_new , color = color_)
+		fig = plt.scatter(df["time"],df[sample],5, facecolor=(.18, .31, .31))
+		plt.ylabel('Absorbance (OD)', fontname="Arial", fontsize=12)
+		plt.xlabel('Time (h)', fontname="Arial", fontsize=12)
+		plt.title("Growth rate curve of "+str(sample), fontname="Arial", fontsize=12)
+		return fig
 
 
 
--- OD_handler/__main__.py
+++ OD_handler/__main__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ### Libraries ###
 import sys
-from OD_handler.OD_handler import gr_plots, parse_data, read_xlsx, sample_outcome, gr_time_format, reshape_gr, vol_correlation, compensation_lm, gr_estimation
+from OD_handler.OD_handler import argument_parser, gr_plots, parse_data, read_xlsx, sample_outcome, time_formater, reshape_dataframe, vol_correlation, compensation_lm, gr_estimation
 from croissance.estimation.outliers import remove_outliers
 import croissance
 from croissance import process_curve
@@ -13,50 +13,57 @@ import os
 import matplotlib.pyplot as plt 
 import matplotlib
 from pandas import Series
+from matplotlib.pyplot import cm
+import argparse
 
+
+import pandas as pd
+from datetime import datetime
+import croissance
+from croissance import process_curve
+from croissance.estimation.outliers import remove_outliers
+import re
+import os
+import matplotlib.pyplot as plt 
+import matplotlib
+import numpy as np
+from scipy.optimize import curve_fit
+from croissance.estimation.util import with_overhangs
+from croissance.estimation import regression
+from pandas import Series
+import subprocess
+import sys
+from scipy import interpolate
+from matplotlib.pyplot import cm
 
 def main():
 
-	#Default, the three options are computed
-	flag_all = True
-	flag_est = False
-	flag_sum = False
-	flag_fig = False
+
+
 	# ----- INPUT INTERPRETATION AND FILE READING ------
 
-	if len(sys.argv) == 2 :
-		
-		if sys.argv[1] == "-h" or sys.argv[1] == "--help" :
-			print('''\nUsage:\n OD_handler [OPTIONS]\n\nOptions:\n --help				Shows the help\n --estimations			Returns estimations in a text file\n --figures			Returns the growth rate curves\n --summary			returns the summary of growth rates\n\nDefault:\n Returns the estimations, the figures and the summary.''')
-			sys.exit(1)
-		elif sys.argv[1] == "--estimations" :
-			flag_est = True
-			flag_all = False
-		elif sys.argv[1] == "--figures" :
-			flag_fig = True
-			flag_all = False
-		elif sys.argv[1] == "--summary" :
-			flag_sum = True
-			flag_all = False
-		elif sys.argv[1] == "--individualplots" :
-			flag_ind = True
-			flag_all = False
-		
-		parse_data()
-		try :
-			df_raw = read_xlsx()
-		except FileNotFoundError :
-			sys.exit("Error!\n parsed file not found")
 
-	else :
-		parse_data()
-		try :
-			df_raw = read_xlsx()
-		except FileNotFoundError :
-			sys.exit("Error!\n parsed file not found")
+	flag_all, flag_est, flag_sum, flag_fig, flag_ind, flag_bioshaker = argument_parser(argv_list= sys.argv)
+	parse_data()
+
+	try :
+		df_raw = read_xlsx()
+
+	except FileNotFoundError :
+		sys.exit("Error!\n parsed file not found")
+	
+
+
+	# ----- LABELLING AND FORMATTING -----
+	
 
 	#Separate data depending on sample purpose (growth rate or volume loss)
 	df_gr, df_vl = sample_outcome("calc.tsv", df_raw)
+
+	#Convert time variable to differential hours for both dataframes
+	df_gr = time_formater(df_gr)
+	df_vl = time_formater(df_vl)
+
 	
 	#look at the amount of different species, this will be used as an argument in the reshape method
 	multiple_species_flag = False
@@ -68,33 +75,49 @@ def main():
 	else :
 
 		pass
-	
+
+
+
+	# ----- CORRELATION AND CORRECTION -----
+
+
 	#Compute correlation for every sample 
-	cor_df, df_gr = vol_correlation(df_gr, df_vl)
-
-
-	#Change time format to hours
-	df_gr = gr_time_format(df_gr)
+	cor_df = vol_correlation(df_gr, df_vl)
 
 	#Compute compensation
 	fig, df_gr = compensation_lm(cor_df, df_gr)
-	plt.savefig("lm_volume_loss.pdf")
+	plt.savefig("lm_volume_loss.png")
+	plt.close()
+	
 
+
+	# ----- DATA RESHAPING FOR CROISSANCE INPUT REQUIREMENTS -----
+
+
+	
 	#Reshape data for croissance input
 	
 	if multiple_species_flag == False :
-		df_gr_final = reshape_gr(df_gr, flag = multiple_species_flag)
+
+		df_gr_final = reshape_dataframe(df_gr, flag = multiple_species_flag)
 		print("a")
+
 	else :
-		df_gr_final, df_gr_final_list = reshape_gr(df_gr, flag = multiple_species_flag)
+		df_gr_final, df_gr_final_list = reshape_dataframe(df_gr, flag = multiple_species_flag)
+
+
+
+	# ----- ESTIMATIONS -----
+
 
 	#Get estimations of every sample
 
 	if flag_all == True :
 		
-		estimations, errors = gr_estimation(df_gr_final)
+		estimations, errors, est_IDs, err_IDs = gr_estimation(df_gr_final)
 		outfile = open("estimations.txt", 'w')
 		
+		#Here would go the estimation writter
 		for est in estimations :
 			print(est,"\n", file = outfile)
 		
@@ -117,28 +140,79 @@ def main():
 			for col in range(len(colnames)):
 				my_series = pd.Series(data = (df_gr_final[colnames[col]]).tolist(), index= df_gr_final[colnames[col].replace("Corrected","time")].tolist())
 				my_series = Series.dropna(my_series)
-				clean_series = remove_outliers(my_series)[0]	#Extract series withoyt outliers
+				clean_series = remove_outliers(my_series)[0]	#Extract series without outliers
 				df = pd.DataFrame({"time":clean_series.index, colnames[col]:clean_series.values})
-				plot = gr_plots(df, colnames[col])
+				plot = gr_plots(df, colnames[col], ind = True)
 		
 		elif flag_ind == False :
-			pass#Create plots by species
+			
+			bioshaker_list = cor_df["bioshaker"].unique()
+			
+			for df_gr_final in df_gr_final_list :
+
+				df_gr_est = df_gr_final.loc[:,~df_gr_final.columns.str.startswith('Raw')]
+				df_gr_est = df_gr_est.loc[:,~df_gr_est.columns.str.startswith('time')]
+				colnames = (df_gr_est.columns.values)
+				bioshaker_label = re.search(r"[_]([BS]\d)[_]",colnames[1]).group(1)
+				plt.figure()
+
+				for col in range(len(colnames)):
+					
+					if "BS1" in colnames[col] :
+						color_ = "blue"
+					else :
+						color_ = "red"
+				
+					my_series = pd.Series(data = (df_gr_final[colnames[col]]).tolist(), index= df_gr_final[colnames[col].replace("Corrected","time")].tolist())
+					my_series = Series.dropna(my_series)
+					clean_series = remove_outliers(my_series)[0]	#Extract series without outliers
+					df = pd.DataFrame({"time":clean_series.index, colnames[col]:clean_series.values})
+					gr_plots(df, colnames[col], color_ = color_)
+
+				plt.savefig(bioshaker_label+"_GR_curve.png")
 
 	if flag_fig == True :
+
 		if flag_ind == True :
+
 			# Get plots for every sample
 			df_gr_est = df_gr_final.loc[:,~df_gr_final.columns.str.startswith('Raw')]
 			df_gr_est = df_gr_est.loc[:,~df_gr_est.columns.str.startswith('time')]
 			colnames = (df_gr_est.columns.values)
 
-		for col in range(len(colnames)):
+			for col in range(len(colnames)):
 				my_series = pd.Series(data = (df_gr_final[colnames[col]]).tolist(), index= df_gr_final[colnames[col].replace("Corrected","time")].tolist())
 				my_series = Series.dropna(my_series)
-				clean_series = remove_outliers(my_series)[0]	#Extract series withoyt outliers
+				clean_series = remove_outliers(my_series)[0]	#Extract series without outliers
 				df = pd.DataFrame({"time":clean_series.index, colnames[col]:clean_series.values})
-				plot = gr_plots(df, colnames[col])
-		else :
-			pass#create plots for every species
+				plot = gr_plots(df, colnames[col],ind = True)
+		
+		elif flag_ind == False :
+			
+			bioshaker_list = cor_df["bioshaker"].unique()
+			
+			for df_gr_final in df_gr_final_list :
+
+				df_gr_est = df_gr_final.loc[:,~df_gr_final.columns.str.startswith('Raw')]
+				df_gr_est = df_gr_est.loc[:,~df_gr_est.columns.str.startswith('time')]
+				colnames = (df_gr_est.columns.values)
+				bioshaker_label = re.search(r"[_]([BS]\d)[_]",colnames[1]).group(1)
+				plt.figure()
+
+				for col in range(len(colnames)):
+					
+					if "BS1" in colnames[col] :
+						color_ = "blue"
+					else :
+						color_ = "red"
+				
+					my_series = pd.Series(data = (df_gr_final[colnames[col]]).tolist(), index= df_gr_final[colnames[col].replace("Corrected","time")].tolist())
+					my_series = Series.dropna(my_series)
+					clean_series = remove_outliers(my_series)[0]	#Extract series without outliers
+					df = pd.DataFrame({"time":clean_series.index, colnames[col]:clean_series.values})
+					gr_plots(df, colnames[col], color_ = color_)
+
+				plt.savefig(bioshaker_label+"_GR_curve.png")
 
 	elif flag_est == True :
 
@@ -159,5 +233,7 @@ def main():
 
 		estimations, errors = gr_estimation(df_gr_final)
 		#Do something with the estimations
+
+	
 	
 

cab6e85c6a355b2391368e68acd80afd5c2e7b1d
Felix Pacheco Pastor <felixpacheco96@gmail.com>
2020-04-03 08:37:05 +0200

    command line runner

---
+++ tecan_od_analyzer-runner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""Convenience wrapper for running bootstrap directly from source tree."""
+
+
+from tecan_od_analyzer import main
+
+
+if __name__ == '__main__':
+    main()

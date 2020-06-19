09ae6dcb2bbff9431c6896d640366d4f00c668fa
felixpacheco <felixpacheco96@gmail.com>
2020-06-19 18:44:24 +0200

    config.py master_doc update

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ copyright = '2020, Felix Pacheco Pastor'
 author = 'Felix Pacheco Pastor'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.8'
+release = '0.0.9'
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,6 +30,9 @@ release = '0.0.8'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+ master_doc = 'index'
+
+
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon', 'recommonmark']
 
 # Add any paths that contain templates here, relative to this directory.
@@ -51,4 +54,4 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static'] 
+html_static_path = ['_static']  

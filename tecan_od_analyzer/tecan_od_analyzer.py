ab89851e6d0c6808ccd8a3f646e5edd4fa2ccfaf
mmattano <57752712+mmattano@users.noreply.github.com>
2020-10-27 10:03:59 +0100

    fix: Remove whitespaces documentation
    Added a comment to describe what is happening below

--- tecan_od_analyzer/tecan_od_analyzer.py
+++ tecan_od_analyzer/tecan_od_analyzer.py
@@ -218,6 +218,8 @@ def sample_outcome(sample_file, df) : #done
 
 	#Open the file containing sample purposes
 	df_calc = pd.read_csv(sample_file, sep="\t")  #Info about the purpose of the sample (growth rate, volume loss compensation, species and drop-out samples)
+	
+	# Remove whitespaces in Sample_IDs
 	df["Sample_ID"] = df["Sample_ID"].str.replace(" ","")
 	df_calc["Sample_ID"] = df_calc["Sample_ID"].str.replace(" ","")
 

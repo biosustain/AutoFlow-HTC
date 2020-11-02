ed798308ae7681fc1af27f713be4a87709298584
mmattano <57752712+mmattano@users.noreply.github.com>
2020-11-02 19:13:14 +0100

    tests/fix: possible fix for the CI Linux build/test break issue
    According to the reported error message, adding the package "wheel" might fix this issue, will have to be tested.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
          "License :: OSI Approved :: MIT License",
          "Operating System :: OS Independent",
     ],
-    install_requires=['numpy','matplotlib','pandas','datetime','argparse','path','xlsxwriter', 'seaborn', 'scipy', 'pycodestyle','sphinx','sphinx_rtd_theme','xlrd'],
+    install_requires=['numpy','matplotlib','pandas','datetime','argparse','path','xlsxwriter', 'seaborn', 'scipy', 'pycodestyle','sphinx','sphinx_rtd_theme','xlrd','wheel'],
     dependency_links=['https://github.com/meono/croissance'],
     include_package_data = True,
 

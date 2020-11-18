fix: adjusted stepwise growth rate calculation
This was done to avoid the error messages that would usually be displayed if were no positive values available for a well

tecan_od_analyzer/tecan_od_analyzer.py
@@ -825,24 +825,25 @@ def step_gr_calculator(df):
             continue
         else:
             previous_val = df.iloc[:, 1][i-1]
-            previous_time = df.iloc[:, 0][i-1]
-            time_val = df.iloc[:, 0][i]
-            gr = (np.log(row_val) - np.log(previous_val)) / \
-                (time_val - previous_time)
-            rates.append(gr)
-            times.append(time_val)
-
-    # fig = plt.scatter(times, rates, 5, facecolor=(.18, .31, .31),
-    #                   label=sample_name)
-    plt.violinplot(rates, positions=[0], showmeans=True, showextrema=True)
-    plt.plot(times, rates, linestyle='-', color=(.18, .31, .31), alpha=.25)
-    plt.ylabel('Growth rate ($h^{-1}$)', fontname="Arial", fontsize=12)
-    plt.xlabel('Time (h)', fontname="Arial", fontsize=12)
-    plt.title("Growth rates of "+sample_name, fontname="Arial", fontsize=12)
-    plt.tight_layout()
-    plt.savefig('Temporary_GR_check/Temporary_GR_check_' + str(sample_name) +
-                "_GRs.png")
-    plt.close()
+            if previous_val < 0 or row_val < 0:
+                continue
+            else:
+                previous_time = df.iloc[:, 0][i-1]
+                time_val = df.iloc[:, 0][i]
+                gr = (np.log(row_val) - np.log(previous_val)) / \
+                    (time_val - previous_time)
+                rates.append(gr)
+                times.append(time_val)
+    if rates:
+        plt.violinplot(rates, positions=[0], showmeans=True, showextrema=True)
+        plt.plot(times, rates, linestyle='-', color=(.18, .31, .31), alpha=.25)
+        plt.ylabel('Growth rate ($h^{-1}$)', fontname="Arial", fontsize=12)
+        plt.xlabel('Time (h)', fontname="Arial", fontsize=12)
+        plt.title("Growth rates of "+sample_name, fontname="Arial", fontsize=12)
+        plt.tight_layout()
+        plt.savefig('Temporary_GR_check/Temporary_GR_check_' + str(sample_name) +
+                    "_GRs.png")
+        plt.close()
     return sample_name, rates, times
 
 
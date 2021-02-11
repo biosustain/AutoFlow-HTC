a756bc2f74d1a5362a500dceee085c6f6550a794
mmattano <57752712+mmattano@users.noreply.github.com>
2021-02-11 10:50:40 +0100

    fix: issues with requirements

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
-        "xcode==0.0.1",
+        # "xcode==0.0.1",
         "wheel==0.36.2",
         "numpy==1.19.5",
         "matplotlib==3.3.3",
